f8e63815d4c7c3c3335d4fb3386cbabed00f76cf
Arseniy <69422628+Pe4eNPe4eNkI@users.noreply.github.com>
2024-03-25 14:13:25 +0500

    Add files via upload

---
+++ src/credit_dml.sql
@@ -0,0 +1,44 @@
+INSERT INTO credit (client_id, opening_date, closing_date, credit_summ, credit_name) 
+VALUES (1, "25.04.2024", "25.04.2026", 4000, "5 credit");
+
+INSERT INTO credit (client_id, opening_date, closing_date, credit_summ, credit_name) 
+VALUES (1, "09.12.2024", "09.12.2026", 1000, "8 credit");
+
+INSERT INTO credit (client_id, opening_date, closing_date, credit_summ, credit_name) 
+VALUES (1, "03.04.2023", "03.04.2026", 400, "6 credit");
+
+INSERT INTO credit (client_id, opening_date, closing_date, credit_summ, credit_name) 
+VALUES (1, "06.03.2024", "06.03.2026", 500, "1 credit");
+
+INSERT INTO credit (client_id, opening_date, closing_date, credit_summ, credit_name) 
+VALUES (1, "17.01.2023", "17.01.2026", 100, "2 credit");
+
+INSERT INTO credit (client_id, opening_date, closing_date, credit_summ, credit_name) 
+VALUES (1, "18.06.2023", "18.06.2026", 300, "5 credit");
+
+INSERT INTO credit (client_id, opening_date, closing_date, credit_summ, credit_name) 
+VALUES (2, "27.04.2023", "27.04.2026", 400, "3 credit");
+
+INSERT INTO credit (client_id, opening_date, closing_date, credit_summ, credit_name) 
+VALUES (3, "29.08.2024", "29.08.2026", 600, "1 credit");
+
+INSERT INTO credit (client_id, opening_date, closing_date, credit_summ, credit_name) 
+VALUES (4, "21.09.2023", "25.09.2026", 5400, "5 credit");
+
+INSERT INTO credit (client_id, opening_date, closing_date, credit_summ, credit_name) 
+VALUES (5, "25.04.2024", "25.04.2027", 1000, "4 credit");
+
+INSERT INTO credit (client_id, opening_date, closing_date, credit_summ, credit_name) 
+VALUES (6, "15.04.2023", "15.04.2026", 6500, "8 credit");
+
+INSERT INTO credit (client_id, opening_date, closing_date, credit_summ, credit_name) 
+VALUES (7, "23.02.2024", "23.02.2025", 1000, "2 credit");
+
+INSERT INTO credit (client_id, opening_date, closing_date, credit_summ, credit_name) 
+VALUES (1, "04.08.2023", "04.08.2024", 4500, "4 credit");
+
+INSERT INTO credit (client_id, opening_date, closing_date, credit_summ, credit_name) 
+VALUES (2, "13.01.2024", "13.01.2029", 1000, "1 credit");
+
+INSERT INTO credit (client_id, opening_date, closing_date, credit_summ, credit_name) 
+VALUES (4, "28.07.2023", "28.07.2026", 2500, "3 credit");
---
+++ src/deposit_dml.sql
@@ -0,0 +1,44 @@
+INSERT INTO deposit (client_id, opening_date, closing_date, deposit_summ, deposit_name) 
+VALUES (1, "25.04.2024", "25.04.2026", 100000, "5 deposit");
+
+INSERT INTO deposit (client_id, opening_date, closing_date, deposit_summ, deposit_name) 
+VALUES (1, "09.12.2024", "09.12.2026", 100000, "8 deposit");
+
+INSERT INTO deposit (client_id, opening_date, closing_date, deposit_summ, deposit_name) 
+VALUES (1, "03.04.2023", "03.04.2026", 500000, "6 deposit");
+
+INSERT INTO deposit (client_id, opening_date, closing_date, deposit_summ, deposit_name) 
+VALUES (1, "06.03.2024", "06.03.2026", 400000, "1 deposit");
+
+INSERT INTO deposit (client_id, opening_date, closing_date, deposit_summ, deposit_name) 
+VALUES (1, "17.01.2023", "17.01.2026", 200000, "2 deposit");
+
+INSERT INTO deposit (client_id, opening_date, closing_date, deposit_summ, deposit_name) 
+VALUES (1, "18.06.2023", "18.06.2026", 300000, "5 deposit");
+
+INSERT INTO deposit (client_id, opening_date, closing_date, deposit_summ, deposit_name) 
+VALUES (2, "27.04.2023", "27.04.2026", 100000, "3 deposit");
+
+INSERT INTO deposit (client_id, opening_date, closing_date, deposit_summ, deposit_name) 
+VALUES (3, "29.08.2024", "29.08.2026", 45000, "1 deposit");
+
+INSERT INTO deposit (client_id, opening_date, closing_date, deposit_summ, deposit_name) 
+VALUES (4, "21.09.2023", "25.09.2026", 540000, "5 deposit");
+
+INSERT INTO deposit (client_id, opening_date, closing_date, deposit_summ, deposit_name) 
+VALUES (5, "25.04.2024", "25.04.2027", 1000000, "4 deposit");
+
+INSERT INTO deposit (client_id, opening_date, closing_date, deposit_summ, deposit_name) 
+VALUES (6, "15.04.2023", "15.04.2026", 3000000, "8 deposit");
+
+INSERT INTO deposit (client_id, opening_date, closing_date, deposit_summ, deposit_name) 
+VALUES (7, "23.02.2024", "23.02.2025", 1000000, "2 deposit");
+
+INSERT INTO deposit (client_id, opening_date, closing_date, deposit_summ, deposit_name) 
+VALUES (1, "04.08.2023", "04.08.2024", 900000, "4 deposit");
+
+INSERT INTO deposit (client_id, opening_date, closing_date, deposit_summ, deposit_name) 
+VALUES (2, "13.01.2024", "13.01.2029", 4000000, "1 deposit");
+
+INSERT INTO deposit (client_id, opening_date, closing_date, deposit_summ, deposit_name) 
+VALUES (4, "28.07.2023", "28.07.2026", 6000000, "3 deposit");
